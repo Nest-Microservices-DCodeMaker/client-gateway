788cbdf8c222284ce25f31d523e31d7355886f43
DelioCoder <davideveriwhere@gmail.com>
2025-08-05 13:03:59 -0500

    UseGuards added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,15 @@
-import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Body, Controller, Get, Inject, Post, Req, UseGuards } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config/services';
+import { catchError } from 'rxjs';
 import { LoginUserDto, RegisterUserDto } from './dto';
+import { AuthGuard } from './guards/auth.guard';
+import { Token, User } from './decorators';
+import { CurrentUser } from './interfaces/current-user.interface';
 
 @Controller('auth')
 export class AuthController {
-  
+
     constructor(
         @Inject(NATS_SERVICE)
         private readonly authClient: ClientProxy
@@ -15,19 +19,31 @@ export class AuthController {
     registerUser(
         @Body() registerUserDto: RegisterUserDto
     ) {
-        return this.authClient.send('auth.register.user', registerUserDto);
+        return this.authClient.send('auth.register.user', registerUserDto).pipe(
+            catchError((err) => {
+                throw new RpcException(err)
+            })
+        )
     }
 
     @Post('login')
     loginUser(
         @Body() loginUserDto: LoginUserDto
     ) {
-        return this.authClient.send('auth.login.user', loginUserDto);
+        return this.authClient.send('auth.login.user', loginUserDto).pipe(
+            catchError((err) => {
+                throw new RpcException(err)
+            })
+        )
     }
 
+    @UseGuards( AuthGuard )
     @Get('verify')
-    verifyUser() {
-        return this.authClient.send('auth.verify.user', {});
+    verifyUser(
+        @User() user: CurrentUser,
+        @Token() token: string
+    ) {
+        return { user, token }
     }
 
 }
