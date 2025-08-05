866a3605c3965b15c50a9c62c11cc1c369628aa2
DelioCoder <davideveriwhere@gmail.com>
2025-08-05 13:03:18 -0500

    RpcCustomException fixed

--- src/common/exceptions/rpc-custom-exception.filter.ts
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -1,4 +1,5 @@
 import { Catch, ArgumentsHost, ExceptionFilter } from '@nestjs/common';
+
 import { RpcException } from '@nestjs/microservices';
 
 @Catch(RpcException)
@@ -7,30 +8,23 @@ export class RpcCustomExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
 
-    const rpcError = exception.getError();
+    const error = exception.getError();
 
+    let status = 500;
+    let message = 'Unexpected error';
+
+    // Si es un objeto con status y message
     if (
-      typeof rpcError === 'object' &&
-      'status' in rpcError &&
-      'message' in rpcError
+      typeof error === 'object' &&
+      'status' in error &&
+      'message' in error
     ) {
-      const status = Number(rpcError.status);
-
-      if (!Number.isInteger(status) || status < 100 || status > 599) {
-        // Status inválido, forzamos 500
-        return response.status(500).json({
-          status: 500,
-          message: rpcError.message || 'Internal Server Error',
-        });
-      }
-
-      return response.status(status).json(rpcError);
+      status = +error.status! || 500;
+      message = error.message as string;
+    } else if (typeof error === 'string') {
+      message = error;
     }
 
-    // Caso default para otros errores
-    response.status(400).json({
-      status: 400,
-      message: typeof rpcError === 'string' ? rpcError : 'Bad Request',
-    });
+    return response.status(status).json({ status, message });
   }
 }
